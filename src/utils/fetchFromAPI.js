c8c30844acccc1ba67683139de3486ea99db445a
Abdelrahman_Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2023-10-12 20:21:01 +0300

    final improve

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '0c1da9d3a0msh4dcec2df3fa369fp116b1fjsn0036a42329e4',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
